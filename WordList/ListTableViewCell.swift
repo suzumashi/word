fef2be6c1d47ad1db74d045bc88f1c45a320ff2c
MashiroSuzuki <9.petitapetit@gmail.com>
2022-05-13 00:25:33 +0900

    Initial Commit

---
+++ WordList/ListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListTableViewCell.swift
+//  WordList
+//
+//  Created by 鈴木ましろ on 2022/05/12.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var englishLabel: UILabel!
+    @IBOutlet var japaneseLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
